7dd2acda315980ab04fd75b537d20394b17ba9ba
Manas <manas.inamdar@students.iiit.ac.in>
2025-06-08 02:47:38 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -6,7 +6,21 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({ path: './env' });
-connectDB();
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT||8000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 8000}`);
+})
+})
+.catch((err) => {
+    // console.error("MONGO DB connection failed", err);
+    app.on('error',(error)=>{
+            console.log("ERRR",error);
+            throw error;
+    // process.exit(1); // Exit the process with failure
+})
+});
 
 
 
@@ -18,9 +32,9 @@ connectDB();
 // (async()=>{
 //     try{
 //         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on('error',(error)=>{
-//             console.log("ERRR",error);
-//             throw error;
+        // app.on('error',(error)=>{
+        //     console.log("ERRR",error);
+        //     throw error;
 //         })
 //         app.listen(process.env.PORT , () => {
 //             console.log(`Server is running on port ${process.env.PORT}`);
