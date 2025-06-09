972efd150118fc02cbedb9b5e5c1b11256f82a30
Manas <manas.inamdar@students.iiit.ac.in>
2025-06-10 04:41:10 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,16 @@
-import { asyncHandler } from '../utils/asyncHandler.js';
-import { ApiError } from '../utils/apiError.js';
-import { User } from '../models/user.model.js';
-import { uploadOnCloudinary } from '../utils/cloudinary.js';
-import { ApiResponse } from '../utils/ApiResponse.js';
+import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+import { User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
+
 
 const registerUser = asyncHandler(async (req, res) => {
+    if (!req.body) {
+        throw new ApiError(400, "Request body is missing");
+    }
     // get user details from frontend
     // validation  - not empty
     // check if user already exists : username or email
@@ -15,16 +21,16 @@ const registerUser = asyncHandler(async (req, res) => {
     // check for user creation
     // return res
 
-    const { fullname, email, username, password } = req.body;
-    console.log("email", email);
+    const { fullName, email, username, password } = req.body;
+    // console.log("email", email);
 
     if (
-        [fullname, email, username, password].some((field) => field?.trim() === "")
+        [fullName, email, username, password].some((field) => field?.trim() === "")
     ) {
         throw new ApiError(400, "All fields are required");
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -32,35 +38,36 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(409, "Username or email already exists");
     }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // console.log("req.files", req.files);
 
-    if (!avatarLocalPath) {
-        throw new ApiError(400, "Avatar is required");
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if (!avatar) {
-        throw new ApiError(500, "Avatar upload failed");
+        throw new ApiError(511, "Avatar upload failed");
     }
 
    const user = await User.create({
-        fullname,
+        fullName,
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
         email,
         password,
         username: username.toLowerCase(),
-
-
     })
 
     const createdUser = await User.findById(user._id).select("-password -refreshToken");
 
     if (!createdUser) {
-        throw new ApiError(500, "User creation failed");
+        throw new ApiError(522, "User creation failed");
     }
 
 return res.status(201).json(
@@ -70,3 +77,4 @@ return res.status(201).json(
 });
 
 export { registerUser }
+
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt";
 
 
 const userSchema = new Schema({
-    name: {
+    username: {
         type: String,
         required: true,
         unique: true,
